06ef1c97e60617883faa3345592eabb82b3fe326
Nima Saghafi <salekt@gmail.com>
2018-01-21 14:16:59 -0500

    initial setup

--- app.js
+++ app.js
@@ -4,14 +4,44 @@ const bodyParser    = require('body-parser');
 const cors    		= require('cors');
 const passport      = require('passport');
 const mongoose      = require('mongoose');
+const config		= require('./config/database');
 
-const app  = express();
-const port = 3000;
+// Connect to DB
+mongoose.connect(config.database);
 
+// On Connect to DB
+mongoose.connection.on('connected', () => {
+	console.log('Connected to database ' + config.database);
+});
+
+// On Error
+mongoose.connection.on('error', () => {
+	console.log('Database Error ' + err);
+});
+
+const app   = express();
+
+const users = require('./routes/users');
+
+// Port #
+const port  = 3000;
+
+// CORS
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser 
+app.use(bodyParser.json());
+app.use('/users', users);
+
+// Index Route
 app.get('/', (req, res) => {
 	res.send('todo');
 })
 
+// Start Server
 app.listen(port, () => {
 	console.log('Server started on port ' + port);
 })
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	database: 'mongodb://localhost:27017/sclera',
+	secret: 'neon_winter'
+}
