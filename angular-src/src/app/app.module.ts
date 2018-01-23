c08ff2a0032a92dd1175630d709d0253485c579d
Nima Saghafi <salekt@gmail.com>
2018-01-23 13:57:30 -0500

    registeration form

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -12,6 +11,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+import { ValidateService } from './services/validate.service';
 
 const appRoutes: Routes = [
   {path: '',          component: HomeComponent},
@@ -37,7 +38,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { validateConfig } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email:    String;
+  username: String;
+  password: String;
 
-  constructor() { }
+
+
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit(){
+    const user = {
+      email:    this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Check if fields are filled in form
+    if(!this.validateService.validateRegister(user)){
+      console.log('all fields not filled in');
+      return false;
+    }
+
+    // Check email validity
+    if(!this.validateService.validateEmail(user.email)){
+      console.log('invalid email provided');
+      return false;
+    }
+  }
 }
